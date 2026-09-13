771f4b4556ee4f237688e518f1669fce1bd56fd8
Muhammad Bilal Khan <muhammadbilalkhan1120@gmail.com>
2026-09-13 15:33:13 +0500

    add temporary admin creation route

--- routes/auth.js
+++ routes/auth.js
@@ -158,4 +158,28 @@ router.post('/admin/login', async (req, res) => {
   }
 });
 
+// Temporary Route to Create Admin
+router.post('/create-admin-once', async (req, res) => {
+  try {
+    const existingAdmin = await User.findOne({ email: "admin@shop.co" });
+    if (existingAdmin) {
+      return res.status(400).json({ message: "Admin already exists!" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash("adminpassword123", salt); // Yahan apna marzi ka password rakh sakte hain
+
+   const adminUser = new User({
+      name: "shop.co",
+      email: "admin@shop.co",
+      password: "shop.co7676",
+      role: "admin"
+    });
+    await adminUser.save();
+    res.status(201).json({ message: "Admin created successfully!" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 module.exports = router;
